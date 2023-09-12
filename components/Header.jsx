ca5a85a7a7e1ea8259da49003a5b1a662b96ca4c
Pedro Figueiredo <pedro.figueiredo35@gmail.com>
2023-09-12 15:43:43 +0100

    main structure with functionality to delete

--- components/Header.jsx
+++
@@ -1,5 +0,0 @@
- import "./Header.css";
- export default function Header(){
-    return <header className="Header">This is the Hader</header>
-}
-
---
+++ src/components/Header.jsx
@@ -0,0 +1,6 @@
+ import "./Header.css";
+
+ export default function Header(){
+    return <header className="Header">This is the Header</header>
+}
+
