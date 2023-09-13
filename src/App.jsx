13edfeb7430f0c791add2c9b0cae8af62fb42c9f
Pedro Figueiredo <pedro.figueiredo35@gmail.com>
2023-09-13 14:54:20 +0100

    extract form to add movie component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import movies from "./data/movies.json";
-
+import AddMovie from "./components/AddMovie";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
@@ -20,69 +20,15 @@ function App() {
     setMoviesToDisplay(newList);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const newMovie = {
-      title: title,
-      rating: rating,
-      imgURL: image,
-    };
-
+  const addNewMovie = (newMovie) => {
     const newList = [newMovie, ...moviesToDisplay];
-
     setMoviesToDisplay(newList);
-    setTitle("");
-    setRating("");
-    setImage("");
-  };
+    };
 
   return (
     <>
       <Header numberOfMovies={moviesToDisplay.length} />
-      <section>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Title:
-            <input
-              type="text"
-              name="title"
-              placeholder="enter the title"
-              value={title}
-              required
-              onChange={(e) => {
-                setTitle(e.target.value);
-              }}
-            />
-          </label>
-          <label>
-            Rating:
-            <input
-              type="number"
-              name="rating"
-              min={1}
-              max={10}
-              placeholder="enter the rating"
-              value={rating}
-              onChange={(e) => {
-                setRating(e.target.value);
-              }}
-            />
-          </label>
-          <label>
-            Image
-            <input
-              type="text"
-              name="image"
-              placeholder="link to your image"
-              value={image}
-              onChange={(e) => {
-                setImage(e.target.value);
-              }}
-            />
-          </label>
-          <button>Create</button>
-        </form>
-      </section>
+      <AddMovie callbackToAddMovie={addNewMovie} />
       <Main movies={moviesToDisplay} callbackToDelete={deleteMovie} />
       <Footer />
     </>
